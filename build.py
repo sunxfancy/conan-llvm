cc9f7fbab50c8d8ab8adf183ca62638a2187b8b7
西风逍遥游 <sun_xfancy@163.com>
2018-12-07 22:30:06 -0800

    upload

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # Keep only Release builds
     filtered_builds = []
     for settings, options, env_vars, build_requires, reference in builder.items:
-        if settings["build_type"] == "Release":
+        if settings["build_type"] == "Release" and settings["arch"] == "x86_64":
             filtered_builds.append([settings, options, env_vars, build_requires])
     builder.builds = filtered_builds
 
